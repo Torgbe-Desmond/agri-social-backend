fbf59256e9bcd3946c85a20272af800e72d4e4e2
Torgbe-Desmond <123torgbe@gmail.com>
2025-07-02 21:10:35 +0000

    initial commit

--- blog/utils/firebase_interactions.py
+++ blog/utils/firebase_interactions.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 # credentials_path = Path(__file__).parent / "file-transfer-app-74625-firebase-adminsdk-4vum9-d106be7043.json"
 _credentials = os.getenv("GOOGLE_CREDENTIALS_JSON")
+
 
 # Initialize Firebase Admin SDK
 if not firebase_admin._apps:
